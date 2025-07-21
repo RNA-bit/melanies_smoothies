7753400cf9ee6e5de295ca7d0e9707acd516db33
RNA-bit <rinatsuchiya@gmail.com>
2025-07-22 04:19:16 +0900

    Update streamlit_app.py
    else

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,11 +52,13 @@ if ingredients_list:
     #st.write(my_insert_stmt)
     #st.stop()
     
-time_to_insert = st.button ('Submit Order')
+    time_to_insert = st.button ('Submit Order')
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
     
         st.success('Your Smoothie is ordered, ' +  name_on_order + '!', icon="✅")
 
+        else:
+        st.warning("Please choose at least one ingredient before submitting.")
 
