076f8036fc34aca0283cd8c236d74f79dd6837f7
RNA-bit <rinatsuchiya@gmail.com>
2025-07-22 13:00:08 +0900

    Update streamlit_app.py
    indent fix again

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,12 +36,12 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-    search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+    	search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
 	# st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 	    
-    st.subheader(fruit_chosen + ' Nutrition Information')
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
-    sf_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+        sf_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -51,8 +51,8 @@ if ingredients_list:
     #st.write(my_insert_stmt)
     #st.stop()
     
-time_to_insert = st.button ('Submit Order')
+    time_to_insert = st.button ('Submit Order')
 
-if time_to_insert:
-    session.sql(my_insert_stmt).collect()
-    st.success('Your Smoothie is ordered, ' +  name_on_order + '!', icon="✅")
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered, ' +  name_on_order + '!', icon="✅")
