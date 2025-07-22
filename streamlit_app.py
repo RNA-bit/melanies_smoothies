ba6ab68023fc73d69278dbf1c05cc29780305691
RNA-bit <rinatsuchiya@gmail.com>
2025-07-22 23:18:56 +0900

    Update streamlit_app.py
    st.write(f"search_on = {search_on}, type = {type(search_on)}")

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
+st.write(f"search_on = {search_on}, type = {type(search_on)}")
+
 #Convert
 pd_df=my_dataframe.to_pandas()
 #st.dataframe(pd_df)
