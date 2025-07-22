bc6c086a2c938f5c866b10d282fdfea16a1380eb
RNA-bit <rinatsuchiya@gmail.com>
2025-07-22 13:15:30 +0900

    Update streamlit_app.py
    indent

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-# Import python packages 
+# Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
@@ -7,7 +7,8 @@ import requests
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
     """Choose the fruits you want in your custom Smoothie!
-    """)
+    """
+)
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothies will be:', name_on_order)
@@ -15,20 +16,15 @@ st.write('The name on your Smoothies will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select (col ('FRUIT_NAME'),col('SEARCH_ON'))
-#st.dataframe(data=my_dataframe, use_container_width=True);
-#st.stop()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+pd_df = my_dataframe.to_pandas()
 
-# covert
-pd_df=my_dataframe.to_pandas()
-#st.dataframe(pd_df)
-#st.stop()
-
-ingredients_list = st.multiselect (
-    'Choose up to 5 ingredients:'
-    , my_dataframe
-    ,max_selections=5
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients:',
+    my_dataframe,
+    max_selections=5
 )
+
 ingredients_string = ' '
 
 if ingredients_list:
