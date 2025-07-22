6e0cb1d793615f6c9ec1c87c03558e6a668a9192
RNA-bit <rinatsuchiya@gmail.com>
2025-07-22 18:36:33 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,14 +25,12 @@ ingredients_list = st.multiselect(
     max_selections=5
 )
 
-ingredients_string = ' '
-
+ingredients_string = ''
 if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
        
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
         st.dataframe(data=fruityvice_response.json(), use_container_width=True)
