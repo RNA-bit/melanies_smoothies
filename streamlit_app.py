ddd61977607249eb5aa06f136ce137cb07a3f9ac
RNA-bit <rinatsuchiya@gmail.com>
2025-07-22 22:30:43 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,8 +29,9 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        ingredients_string += fruit_chosen + ' '
+        st.subheaher(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
         
         
